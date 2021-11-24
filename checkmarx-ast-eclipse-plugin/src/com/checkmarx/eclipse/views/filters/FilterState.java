bd1fc4fa6535bcb22659f8f69a0750c248b24ddc
hmmachadocx <hugo.machado@checkmarx.com>
2021-11-24 15:24:01 +0000

    AST-7203 - Eclipse - Store project, branch and scanId in workspace
    - Stored current project and current scan id in the ConfigurationScope
    which allows to share preferences between workspaces;
    
    - Code cleaning: removed unused variables and empty lines from the
    methods' beginning

--- checkmarx-ast-eclipse-plugin/src/com/checkmarx/eclipse/views/filters/FilterState.java
+++ checkmarx-ast-eclipse-plugin/src/com/checkmarx/eclipse/views/filters/FilterState.java
@@ -1,5 +1,6 @@
 package com.checkmarx.eclipse.views.filters;
 
+import com.checkmarx.eclipse.views.GlobalSettings;
 
 public class FilterState {
 
@@ -8,7 +9,17 @@ public class FilterState {
 	public static boolean low = false;
 	public static boolean info = false;
 	public static boolean groupBySeverity = true;
-	public static boolean groupByQueryName = false;
+	public static boolean groupByQueryName = true;
+	
+	
+	public static void loadFiltersFromSettings() {
+		high = Boolean.parseBoolean(GlobalSettings.getFromPreferences(Severity.HIGH.name(), "true"));
+		medium = Boolean.parseBoolean(GlobalSettings.getFromPreferences(Severity.MEDIUM.name(), "true"));
+		low = Boolean.parseBoolean(GlobalSettings.getFromPreferences(Severity.LOW.name(), "false"));
+		info = Boolean.parseBoolean(GlobalSettings.getFromPreferences(Severity.INFO.name(), "false"));
+		groupBySeverity = Boolean.parseBoolean(GlobalSettings.getFromPreferences(Severity.GROUP_BY_SEVERITY.name(), "true"));
+		groupByQueryName = Boolean.parseBoolean(GlobalSettings.getFromPreferences(Severity.GROUP_BY_QUERY_NAME.name(), "true"));
+	}
 	
 	/**
 	 * Change severity state
@@ -16,25 +27,30 @@ public class FilterState {
 	 * @param severity
 	 */
 	public static void setState(Severity severity) {
-		
 		switch(severity) {
 			case HIGH:
 				high = !high;
+				GlobalSettings.storeInPreferences(Severity.HIGH.name(), String.valueOf(high));
 				break;
 			case MEDIUM:
 				medium = !medium;
+				GlobalSettings.storeInPreferences(Severity.MEDIUM.name(), String.valueOf(medium));
 				break;
 			case LOW:
 				low = !low;
+				GlobalSettings.storeInPreferences(Severity.LOW.name(), String.valueOf(low));
 				break;
 			case INFO:
 				info = !info;
+				GlobalSettings.storeInPreferences(Severity.INFO.name(), String.valueOf(info));
 				break;
 			case GROUP_BY_SEVERITY:
 				groupBySeverity = !groupBySeverity;
+				GlobalSettings.storeInPreferences(Severity.GROUP_BY_SEVERITY.name(), String.valueOf(groupBySeverity));
 				break;
 			case GROUP_BY_QUERY_NAME:
 				groupByQueryName = !groupByQueryName;
+				GlobalSettings.storeInPreferences(Severity.GROUP_BY_QUERY_NAME.name(), String.valueOf(groupByQueryName));
 				break;
 		}
 	}
@@ -46,7 +62,6 @@ public class FilterState {
 	 * @return
 	 */
 	public static boolean isSeverityEnabled(String severity) {
-				
 		switch(Severity.getSeverity(severity)) {
 			case HIGH: return high;
 			case MEDIUM: return medium;
@@ -68,6 +83,6 @@ public class FilterState {
 		low = false;
 		info = false;
 		groupBySeverity = true;
-		groupByQueryName = false;
+		groupByQueryName = true;
 	}
 }
