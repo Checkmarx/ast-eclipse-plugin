e8771800a9693e2d03cba094031262af3e09ea09
hmmachadocx <hugo.machado@checkmarx.com>
2021-12-09 11:19:00 +0000

    AST-7511 - Eclipse - Add logging to Error Log
    - Removed unused ScanConfig class

--- checkmarx-ast-eclipse-plugin/src/com/checkmarx/eclipse/utils/PluginConstants.java
+++ checkmarx-ast-eclipse-plugin/src/com/checkmarx/eclipse/utils/PluginConstants.java
@@ -4,6 +4,32 @@ public class PluginConstants {
 	public static final String SAST = "sast";
 	public static final String SCA_DEPENDENCY = "dependency";
 	public static final String KICS_INFRASTRUCTURE = "infrastructure";
-	public static final String RETRIEVING_RESULTS_FOR_SCAN = "Retrieving results for scan id: %s";
+	public static final String RETRIEVING_RESULTS_FOR_SCAN = "Retrieving results for scan id %s...";
 	public static final String COMBOBOX_SCAND_ID_PLACEHOLDER = "Select project or paste a Scan Id here and hit Enter.";
+	public static final String COMBOBOX_SCAND_ID_GETTING_SCANS = "Getting scans for the project...";
+	public static final String COMBOBOX_SCAND_ID_NO_SCANS_AVAILABLE = "No scans available.";
+	
+	/******************************** LOG VIEW: ERRORS ********************************/
+	public static final String ERROR_AUTHENTICATING_AST = "An error occurred while trying to authenticate to AST: %s";
+	public static final String ERROR_GETTING_SCAN_INFO = "An error occurred while getting scan information: %s";
+	public static final String ERROR_GETTING_PROJECTS = "An error occurred while getting projects: %s";
+	public static final String ERROR_GETTING_BRANCHES = "An error occurred while getting branches for project id %s: %s";
+	public static final String ERROR_GETTING_SCANS = "An error occurred while getting scans for project id %s in branch %s: %s";
+	public static final String ERROR_GETTING_RESULTS = "An error occurred while getting results for scan id %s: %s";
+	public static final String ERROR_OPENING_FILE = "An error occurred while opening file: %s";
+	public static final String ERROR_FINDING_FILE = "An error occurred while finding file in workspace: %s";
+	
+	/******************************** LOG VIEW: INFO ********************************/
+	public static final String INFO_AUTHENTICATION_STATUS = "Authentication Status: %s";
+	public static final String INFO_FETCHING_RESULTS = "Fetching results for scan id %s...";
+	public static final String INFO_SCAN_RESULTS_COUNT = "Scan results: %d";
+	public static final String INFO_GETTING_SCAN_INFO = "Getting scan info for scan id %s...";
+	public static final String INFO_RESULTS_ALREADY_RETRIEVED = "Reverse selection not triggered. Results for scan id %s already retrieved.";
+	public static final String INFO_CHANGE_SCAN_EVENT_NOT_TRIGGERED = "Change scan id event not triggered. Request already running: %s. Scan id results already retrieved: %s";
+	public static final String INFO_CHANGE_BRANCH_EVENT_NOT_TRIGGERED = "Change branch event not triggered. Branch already selected";
+	public static final String INFO_CHANGE_PROJECT_EVENT_NOT_TRIGGERED = "Change project event not triggered. Project already selected";
+	
+	/******************************** TREE MESSAGES ********************************/
+	public static final String TREE_PROVIDED_SCAN_ID_DOES_NOT_EXIST = "Provided scan id %s does not exist";
+	public static final String TREE_INVALID_SCAN_ID_FORMAT = "Invalid scan id format.";
 }
