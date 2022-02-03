786bc77c04316058aeeadf43260607b8dc70cee6
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2022-02-03 11:31:40 +0000

    use correct color and fill each row in hover

--- checkmarx-ast-eclipse-plugin/src/com/checkmarx/eclipse/views/HoverListener.java
+++ checkmarx-ast-eclipse-plugin/src/com/checkmarx/eclipse/views/HoverListener.java
@@ -2,18 +2,19 @@ package com.checkmarx.eclipse.views;
 
 import java.util.List;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseTrackListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGBA;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.themes.ITheme;
+import org.eclipse.swt.widgets.Display;
 
 public class HoverListener implements MouseTrackListener {
 
-	private static final String HOVER_COLOR_KEY = "org.eclipse.jdt.ui.interfaceHighlighting";
 	private final List<Control> controls;
 	private final Color defaultColor;
+	private Color customColor;
 
 	public HoverListener(List<Control> controls) {
 		this.controls = controls;
@@ -22,16 +23,22 @@ public class HoverListener implements MouseTrackListener {
 
 	@Override
 	public void mouseEnter(MouseEvent arg0) {
-		ITheme currentTheme = PlatformUI.getWorkbench().getThemeManager().getCurrentTheme();
-		Color themeColor = currentTheme.getColorRegistry().get(HOVER_COLOR_KEY);
-		Color c = new Color(themeColor.getRed(), themeColor.getGreen(), themeColor.getBlue(), 32);
-		controls.forEach(control -> control.setBackground(c));		
+		Color themeColor = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_SELECTION);
+		float[] hsba = themeColor.getRGBA().getHSBA();
+		customColor = new Color(new RGBA(hsba[0], 0.10f, hsba[2], hsba[3]));
+		controls.forEach(control -> {
+			control.setBackground(customColor);
+		});		
 	}
 
 	@Override
 	public void mouseExit(MouseEvent arg0) {
 		if (defaultColor != null) {
-			controls.forEach(control -> control.setBackground(defaultColor));
+			controls.forEach(control -> {
+				control.setBackground(defaultColor);	
+			});
+			customColor.dispose();
+			customColor = null;
 		}		
 	}
 
