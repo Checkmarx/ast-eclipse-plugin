d5bc5ec760ef8520f089c47ba8583a2972ff9574
elchananarb <elchanan.arbiv@checkmarx.com>
2024-12-03 18:50:07 +0200

    revert

--- checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/ui/BaseUITest.java
+++ checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/ui/BaseUITest.java
@@ -23,47 +23,47 @@ import checkmarx.ast.eclipse.plugin.tests.common.Environment;
 public abstract class BaseUITest {
 
 	private static final String INFO_SUCCESSFUL_CONNECTION = "Successfully authenticated to Checkmarx One server!";
-
+	
 	protected static final String ASSERT_FILTER_ACTIONS_IN_TOOLBAR = "All filter actions must be in the tool bar";
-
+	
 	protected static final String TAB_WINDOW = "Window";
-
+	
 	protected static final String ITEM_SHOW_VIEW = "Show View";
 	protected static final String ITEM_PREFERENCES = "Preferences";
 	protected static final String ITEM_OTHER = "Other...";
 	protected static final String ITEM_CHECKMARX = "Checkmarx";
 	protected static final String ITEM_CHECKMARX_AST = "Checkmarx One";
 	protected static final String ITEM_CHECKMARX_AST_SCAN = "Checkmarx One Scan";
-
-	protected static final String LABEL_CX_TEST_SCAN = "Scan Id:";
-
+	
+	protected static final String LABEL_SCAN_ID = "Scan Id:";
+	
 	protected static final String BTN_OPEN = "Open";
 	protected static final String BTN_APPLY = "Apply";
 	protected static final String BTN_TEST_CONNECTION = "Test Connection";
 	protected static final String BTN_OK = "OK";
 	protected static final String BTN_APPLY_AND_CLOSE = "Apply and Close";
-
+	
 	protected static final String SHELL_AUTHENTICATION = "Authentication";
-
+	
 	protected static final String VIEW_CHECKMARX_AST_SCAN = "Checkmarx One Scan";
-
+	
 	protected static SWTWorkbenchBot _bot;
 	private static boolean eclipseProjectExist = false;
-
+	
 	protected static boolean _cxSettingsDefined = false;
-
+	
 	@BeforeClass
 	public static void beforeClass() throws Exception {
 		// Needed to set CI environment keyboard layout
-		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
+		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US"; 
 
 		// Used to decrease tests velocity
 		SWTBotPreferences.PLAYBACK_DELAY = 100;
-
-		SWTBotPreferences.TIMEOUT = 8000;
+		
+		SWTBotPreferences.TIMEOUT = 15000;
 
 		_bot = new SWTWorkbenchBot();
-
+				
 		if(!eclipseProjectExist) {
 			createEclipseProject();
 			eclipseProjectExist = true;
@@ -73,17 +73,17 @@ public abstract class BaseUITest {
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@AfterClass
 	public static void sleep() {
 		_bot.sleep(2000);
 	}
-
-
+	
+	
 	protected static void sleep(long millis) {
 		_bot.sleep(millis);
 	}
-
+	
 	/**
 	 * Used to get the workbench focus back and avoid "widget was null" error message in the CI environment
 	 */
@@ -94,14 +94,14 @@ public abstract class BaseUITest {
 			}
 		});
 	}
-
+	
 	/**
 	 * Set up checkmarx plugin
-	 *
+	 * 
 	 * 		-> Set credentials
 	 * 		-> Test connection
 	 * 		-> Add checkmarx plugin
-	 *
+	 * 
 	 * @throws TimeoutException
 	 */
 	protected void setUpCheckmarxPlugin(boolean ignoreWrongScanValidation) throws TimeoutException {
@@ -110,9 +110,9 @@ public abstract class BaseUITest {
 
 		// Add Checkmarx One Plugin
 		addCheckmarxPlugin(true);
-
+		
 		preventWidgetWasNullInCIEnvironment();
-
+		
 		if(!ignoreWrongScanValidation) {
 			// Test incorrect Scan ID format
 			_bot.comboBox(2).setText("invalid-scan-id");
@@ -123,34 +123,34 @@ public abstract class BaseUITest {
 			assertEquals("The tree must contain one row with an error message", _bot.tree(1).rowCount(), 1);
 			assertEquals("An incorrect scanId format message must be displayed", PluginConstants.TREE_INVALID_SCAN_ID_FORMAT, _bot.tree(1).cell(0, 0));
 		}
-
+		
 		// clear the view before getting the scan id
 		_bot.viewByTitle(VIEW_CHECKMARX_AST_SCAN).viewMenu().menu(PluginConstants.TOOLBAR_ACTION_CLEAR_RESULTS).click();
-
+		
 		sleep(1000);
-
+		
 		// type a valid and existing Scan ID
 		typeValidScanID();
 
-		assertEquals("The tree must contain one row", _bot.tree().rowCount(), 1);
+		assertEquals("The tree must contain one row", _bot.tree().rowCount(), 1);		
 		boolean retrievingOrRetrievedResults = _bot.tree(1).cell(0, 0).contains(Environment.SCAN_ID);
 		assertTrue("The plugin should have or should be retrieving results", retrievingOrRetrievedResults);
 
 		waitWhileTreeNodeEqualsTo(String.format(PluginConstants.RETRIEVING_RESULTS_FOR_SCAN, Environment.SCAN_ID));
-
+		
 		assertTrue("The plugin should retrieve results", _bot.tree(1).cell(0, 0).startsWith(Environment.SCAN_ID));
 	}
 
 	/**
 	 * Test successful connection
-	 *
+	 * 
 	 * @throws TimeoutException
 	 */
 	protected void testSuccessfulConnection(boolean openFromInitialPanel) throws TimeoutException {
 		preventWidgetWasNullInCIEnvironment();
-
+		
 		if(_cxSettingsDefined) return;
-
+		
 		if(!openFromInitialPanel) {
 			_bot.menu(TAB_WINDOW).menu(ITEM_PREFERENCES).click();
 			_bot.shell(ITEM_PREFERENCES).activate();
@@ -160,43 +160,43 @@ public abstract class BaseUITest {
 		_bot.sleep(1000);
 
 		_bot.shell(ITEM_PREFERENCES).setFocus(); // Need to set focus to avoid failing in CI environment
-
+		
 		_bot.textWithLabel(PluginConstants.PREFERENCES_API_KEY).setText(Environment.API_KEY);
 
 		_bot.button(BTN_APPLY).click();
 		_bot.button(BTN_TEST_CONNECTION).click();
-
+		
 		//Do waitUntil Method to get text from text(6)
 		waitForConnectionResponse();
-
+			
 		_bot.shell(ITEM_PREFERENCES).setFocus(); // Need to set focus to avoid failing in CI environment
 		_bot.button(BTN_APPLY_AND_CLOSE).click();
 
 		_cxSettingsDefined = true;
 	}
-
+	
 
 	/**
 	 * Add Checkmarx plugin in the show view perspective
-	 *
-	 * @throws TimeoutException
+	 * 
+	 * @throws TimeoutException 
 	 */
 	protected void addCheckmarxPlugin(boolean waitUntilPluginEnable) throws TimeoutException {
 		preventWidgetWasNullInCIEnvironment();
-
+		
 		_bot.menu(TAB_WINDOW).menu(ITEM_SHOW_VIEW).menu(ITEM_OTHER).click();
 		_bot.shell(ITEM_SHOW_VIEW).activate();
 		_bot.tree().expandNode(ITEM_CHECKMARX).select(ITEM_CHECKMARX_AST_SCAN);
 		_bot.button(BTN_OPEN).click();
-
+		
 		if(waitUntilPluginEnable) {
-			waitUntilBranchComboIsEnabled();
+			waitUntilBranchComboIsEnabled();	
 		}
 	}
-
+	
 	/**
 	 * Wait while tree node equals to a a specific message. Fails after 10 retries
-	 *
+	 * 
 	 * @param nodeText
 	 * @throws TimeoutException
 	 */
@@ -218,22 +218,22 @@ public abstract class BaseUITest {
 			throw new TimeoutException("Timeout after 5000ms. Scan results should be retrieved");
 		}
 	}
-
+	
 	/**
 	 * Wait until branch combobox is enabled
-	 *
+	 * 
 	 * @throws TimeoutException
 	 */
 	protected static void waitUntilBranchComboIsEnabled() throws TimeoutException {
 		preventWidgetWasNullInCIEnvironment();
-
+		
 		boolean emptyScanId = _bot.comboBox(2).getText().isEmpty() || _bot.comboBox(2).getText().equals(PluginConstants.COMBOBOX_SCAND_ID_PLACEHOLDER);
 		boolean projectNotSelected =_bot.comboBox(0).getText().isEmpty() || _bot.comboBox(0).getText().equals("Select a project");
-
+		
 		if(emptyScanId || projectNotSelected) {
 			return;
 		}
-
+		
 		int retryIdx = 0;
 
 		while (!_bot.comboBox(1).isEnabled()) {
@@ -250,18 +250,18 @@ public abstract class BaseUITest {
 		if (retryIdx == 10) {
 			emptyScanId = _bot.comboBox(2).getText().isEmpty() || _bot.comboBox(2).getText().equals(PluginConstants.COMBOBOX_SCAND_ID_PLACEHOLDER);
 			projectNotSelected = _bot.comboBox(0).getText().isEmpty() || _bot.comboBox(0).getText().equals("Select a project");
-
+			
 			if(emptyScanId || projectNotSelected) {
 				return;
 			}
-
+			
 			throw new TimeoutException("Timeout after 5000ms. Branches' combobox must be enabled");
 		}
 	}
-
+	
 	/**
 	 * Wait while tree node equals to a a specific message. Fails after 10 retries
-	 *
+	 * 
 	 * @param nodeText
 	 * @throws TimeoutException
 	 */
@@ -280,31 +280,22 @@ public abstract class BaseUITest {
 			throw new TimeoutException("Connection validation timeout after 10000ms.");
 		}
 	}
-
+	
 
 	/**
 	 * Type a valid Scan ID to get results
-	 *
-	 * @throws TimeoutException
+	 * 
+	 * @throws TimeoutException 
 	 */
 	private void typeValidScanID() throws TimeoutException {
 		preventWidgetWasNullInCIEnvironment();
-
-		String scanId = System.getenv("SCAN_ID");
-		String scanId2 = System.getenv("CX_TEST_SCAN");
-		String scanId3 = Environment.SCAN_ID;
-
-		if (scanId == null || scanId2 == null || scanId3 == null) {
-			throw new IllegalArgumentException("Environment variable Environment.SCAN_ID is not set. Value: " + scanId
-			+ "Environment variable SCAN_ID2 is not set. Value: "  + scanId2 + "Environment variable SCAN_ID3 is not set. Value: " + scanId3);
-		}
-
+		
 		_bot.comboBox(2).setText(Environment.SCAN_ID);
 		_bot.comboBox(2).pressShortcut(Keystrokes.LF);
-
+		
 		waitUntilBranchComboIsEnabled();
 	}
-
+	
 	/**
 	 * Create a eclipse project
 	 */
@@ -314,11 +305,11 @@ public abstract class BaseUITest {
 		shell.activate();
 		_bot.tree().select("Project");
 		_bot.button("Next >").click();
-
-
+		
+ 
 		_bot.textWithLabel("Project name:").setText("MyFirstProject");
 		_bot.button("Finish").click();
-
+		
 		_bot.menu("File").menu("New").menu("File").click();
 		_bot.textWithLabel("File name:").setText("Dockerfile");
 		_bot.tree().select(0);
