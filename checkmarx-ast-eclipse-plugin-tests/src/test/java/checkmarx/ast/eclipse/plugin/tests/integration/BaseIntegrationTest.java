b625d980a28887218ef83a5961ce5e49e0c39004
elchananarb <elchanan.arbiv@checkmarx.com>
2025-01-23 16:14:46 +0200

    v1

--- checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/integration/BaseIntegrationTest.java
+++ checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/integration/BaseIntegrationTest.java
@@ -1,32 +1,53 @@
 package checkmarx.ast.eclipse.plugin.tests.integration;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.checkmarx.ast.wrapper.CxConfig;
 import com.checkmarx.ast.wrapper.CxWrapper;
+import com.checkmarx.eclipse.utils.PluginConstants;
 import checkmarx.ast.eclipse.plugin.tests.common.Environment;
 
-public class BaseIntegrationTest {
+public abstract class BaseIntegrationTest {
     
-    protected static final Logger log = LoggerFactory.getLogger(BaseIntegrationTest.class);
-    protected CxWrapper wrapper;
+    protected static final String VALID_SCAN_ID = Environment.SCAN_ID;
+    protected static final String INVALID_SCAN_ID = "invalid-scan-id";
+    protected static final String VALID_API_KEY = Environment.API_KEY;
+    
+    @Mock
+    protected Logger mockLogger;
+    
+    protected CxWrapper cxWrapper;
+    protected static boolean initialized = false;
+    
+    @BeforeClass
+    public static void setUpClass() {
+        // Global test setup if needed
+    }
     
     @Before
     public void setUp() throws Exception {
-        CxConfig config = CxConfig.builder()
-            .apiKey(Environment.API_KEY)
-            .build();
-            
-        wrapper = new CxWrapper(config, log);
-    }
-    
-    protected void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            // Ignore
+        MockitoAnnotations.openMocks(this);
+        if (!initialized) {
+            initializeCxWrapper();
+            initialized = true;
         }
     }
-} 
+    
+    protected void initializeCxWrapper() throws Exception {
+        CxConfig config = CxConfig.builder()
+                .apiKey(VALID_API_KEY)
+                .build();
+        cxWrapper = new CxWrapper(config, mockLogger);
+    }
+    
+    protected void reinitializeCxWrapper(String apiKey) throws Exception {
+        CxConfig config = CxConfig.builder()
+                .apiKey(apiKey)
+                .build();
+        cxWrapper = new CxWrapper(config, mockLogger);
+    }
+}
