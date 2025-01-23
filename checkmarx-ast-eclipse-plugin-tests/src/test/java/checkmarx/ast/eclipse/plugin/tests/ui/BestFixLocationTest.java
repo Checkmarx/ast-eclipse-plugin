859c6c9ef348f2081979f23f73394505a28d4e05
elchananarb <elchanan.arbiv@checkmarx.com>
2025-01-23 12:35:34 +0200

    Update BestFixLocationTest.java

--- checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/ui/BestFixLocationTest.java
+++ checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/ui/BestFixLocationTest.java
@@ -1,37 +1,37 @@
-// package checkmarx.ast.eclipse.plugin.tests.ui;
+package checkmarx.ast.eclipse.plugin.tests.ui;
 
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 
-// import java.util.concurrent.TimeoutException;
+import java.util.concurrent.TimeoutException;
 
-// import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-// import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-// import com.checkmarx.eclipse.utils.PluginConstants;
+import com.checkmarx.eclipse.utils.PluginConstants;
 
-// //@RunWith(SWTBotJunit4ClassRunner.class)
-// public class BestFixLocationTest extends BaseUITest{
+//@RunWith(SWTBotJunit4ClassRunner.class)
+public class BestFixLocationTest extends BaseUITest{
 
-// 	//@Test
-// 	public void testBestFixLocation() throws TimeoutException {
-// //		setUpCheckmarxPlugin(true);
-// //		SWTBotTreeItem firstNode = getFirstResultNode();
-// //		firstNode.select();
-// //		sleep(3000);
-// //		String BFLText = _bot.textWithId(PluginConstants.BEST_FIX_LOCATION).getText();
-// //		assertTrue(BFLText.equals(PluginConstants.BFL_FOUND) || BFLText.equals(PluginConstants.BFL_NOT_FOUND));	
-// //		_bot.viewByTitle(VIEW_CHECKMARX_AST_SCAN).close();
-// 	}
+	//@Test
+	public void testBestFixLocation() throws TimeoutException {
+//		setUpCheckmarxPlugin(true);
+//		SWTBotTreeItem firstNode = getFirstResultNode();
+//		firstNode.select();
+//		sleep(3000);
+//		String BFLText = _bot.textWithId(PluginConstants.BEST_FIX_LOCATION).getText();
+//		assertTrue(BFLText.equals(PluginConstants.BFL_FOUND) || BFLText.equals(PluginConstants.BFL_NOT_FOUND));	
+//		_bot.viewByTitle(VIEW_CHECKMARX_AST_SCAN).close();
+	}
 	
 
-// 	private SWTBotTreeItem getFirstResultNode() {
-// 		String firstNodeName = _bot.tree().cell(0, 0);
-// 		SWTBotTreeItem node = _bot.tree().getTreeItem(firstNodeName);
-// 		while(!node.getNodes().isEmpty()) {
-// 			node = node.expand().getNode(0);
-// 		}
-// 		return node;
-// 	}
-// }
+	private SWTBotTreeItem getFirstResultNode() {
+		String firstNodeName = _bot.tree().cell(0, 0);
+		SWTBotTreeItem node = _bot.tree().getTreeItem(firstNodeName);
+		while(!node.getNodes().isEmpty()) {
+			node = node.expand().getNode(0);
+		}
+		return node;
+	}
+}
