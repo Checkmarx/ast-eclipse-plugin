a7d902a89768c1042e20f9381b17db8cec27bc11
Aniket Shinde <aniket.shinde@checkmarx.com>
2025-08-04 11:35:42 +0530

    Update BaseUITest.java
    SWTBotPreference timout increased

--- checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/ui/BaseUITest.java
+++ checkmarx-ast-eclipse-plugin-tests/src/test/java/checkmarx/ast/eclipse/plugin/tests/ui/BaseUITest.java
@@ -60,7 +60,7 @@ public abstract class BaseUITest {
 		// Used to decrease tests velocity
 		SWTBotPreferences.PLAYBACK_DELAY = 500;
 		
-		SWTBotPreferences.TIMEOUT = 40000;
+		SWTBotPreferences.TIMEOUT = 60000;
 
 		_bot = new SWTWorkbenchBot();
 				
